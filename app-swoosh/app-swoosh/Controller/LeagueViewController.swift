bac037788d66810ca4aff31ed1fe554ef716ba15
Madhurr <madhurjain874@gmail.com>
2019-04-15 17:32:11 +0530

    last commit

---
+++ app-swoosh/app-swoosh/Controller/LeagueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueViewController.swift
+//  app-swoosh
+//
+//  Created by Madhur on 15/04/19.
+//  Copyright © 2019 Madhur. All rights reserved.
+//
+
+import UIKit
+
+class LeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-swoosh/app-swoosh/Controller/SkillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SkillViewController.swift
+//  app-swoosh
+//
+//  Created by Madhur on 15/04/19.
+//  Copyright © 2019 Madhur. All rights reserved.
+//
+
+import UIKit
+
+class SkillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
